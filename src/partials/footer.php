75f4286bfd410c8bbe968db1ce0747d30bebf4c1
rebs-code <jack.rebussi@gmail.com>
2024-01-15 18:15:26 +0100

    feat: footer

--- src/partials/footer.php
+++ src/partials/footer.php
@@ -1,8 +1,10 @@
-<footer>
-    <p>This is the footer.</p>
-    <ul>
-        <li class="social-icon"><a href="https://twitter.com">Twitter</a></li>
-        <li class="social-icon"><a href="https://facebook.com">Facebook</a></li>
-        <li class="social-icon"><a href="https://instagram.com">Instagram</a></li>
+<footer class="d-flex flex-column align-items-center">
+    <p>Follow us!</p>
+    <ul class="d-flex mx-auto social-list">
+        <li class="social-icon"><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+        <li class="social-icon"><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+        <li class="social-icon"><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+        <li class="social-icon"><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+        <li class="social-icon"><a href="#"><i class="fa-brands fa-tiktok"></i></a></li>
     </ul>
 </footer>
