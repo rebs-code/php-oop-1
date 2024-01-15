04fda94ceab1938b48a1779789e9fb3a91972b0d
rebs-code <jack.rebussi@gmail.com>
2024-01-15 18:59:49 +0100

    feat: fixed style

--- src/partials/header.php
+++ src/partials/header.php
@@ -1,10 +1,13 @@
-<header class="d-flex align-items-center justify-content-between p-3">
-    <h1>PHP Movies</h1>
-    <nav class="col-4">
-        <ul class="d-flex mb-0">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
+<header>
+    <div class="container d-flex align-items-center justify-content-between">
+        <h1>PHP Movies</h1>
+        <nav class="col-4 d-flex">
+            <ul class="d-flex mb-0 ms-auto">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+
 </header>
--- src/partials/main.php
+++ src/partials/main.php
@@ -4,22 +4,28 @@
         // Import the database and fetch the movie data
         include __DIR__ . '../../../database.php';
 
-        foreach ($movies as $movie) {
-            echo '<div class="movie">';
-            echo '<h4>' . $movie->getTitle() . '</h4>';
-            echo '<p>' . $movie->year . '</p>';
-            echo '<p>' . $movie->genre->genre1 . '</p>';
-            if ($movie->genre->genre2 !== null) {
-                echo '<p>' . $movie->genre->genre2 . '</p>';
-            };
-            if ($movie->genre->genre3 !== null) {
-                echo '<p>' . $movie->genre->genre3 . '</p>';
-            };
-            echo '<p>' . $movie->director . '</p>';
-            echo '</div>';
-        }
+        foreach ($movies as $movie) :
+        ?>
+            <div class="movie">
+                <div class="title-div">
+                    <h4><?= $movie->getTitle() ?></h4>
+                </div>
 
-
+                <p>Year: <?= $movie->year ?></p>
+                <p>Genre:</p>
+                <div class="d-flex justify-content-around">
+                    <p><?= $movie->genre->genre1 ?></p>
+                    <?php if ($movie->genre->genre2 !== null) : ?>
+                        <p><?= $movie->genre->genre2 ?></p>
+                    <?php endif; ?>
+                    <?php if ($movie->genre->genre3 !== null) : ?>
+                        <p><?= $movie->genre->genre3 ?></p>
+                    <?php endif; ?>
+                </div>
+                <p><?= $movie->director ?></p>
+            </div>
+        <?php
+        endforeach;
         ?>
     </div>
 </main>
