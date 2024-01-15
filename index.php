bc5781581504741adecd6fc9b1e851d4bbb8c124
rebs-code <jack.rebussi@gmail.com>
2024-01-15 17:56:46 +0100

    add: some style to header

--- index.php
+++ index.php
@@ -16,6 +16,8 @@ include __DIR__ . '/src/models/Genre.php';
     <title>PHP Movies</title>
     <!-- Bootstrap CND -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- my css -->
+    <link rel="stylesheet" href="./src/css/style.css">
 </head>
 
 <body>
--- src/partials/header.php
+++ src/partials/header.php
@@ -1,7 +1,7 @@
-<header class="d-flex">
+<header class="d-flex align-items-center justify-content-between p-3">
     <h1>PHP Movies</h1>
-    <nav>
-        <ul>
+    <nav class="col-4">
+        <ul class="d-flex mb-0">
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Contact</a></li>
