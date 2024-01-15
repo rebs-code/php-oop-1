1579ab18779275bcf7d5c10163c2be13d947c2d0
rebs-code <jack.rebussi@gmail.com>
2024-01-15 16:13:48 +0100

    feat: getTitle getter for Movie class

--- index.php
+++ index.php
@@ -2,17 +2,23 @@
 
 class Movie
 {
+    //create properties
     public $title;
     public $year;
     public $genre;
     public $director;
 
-
+    //create a constructor
     public function __construct($title, $year, $genre, $director)
     {
         $this->title = $title;
         $this->year = $year;
         $this->genre = $genre;
         $this->director = $director;
+    }
+    //create a getter for the title
+    public function getTitle()
+    {
+        return $this->title;
     }
 }
