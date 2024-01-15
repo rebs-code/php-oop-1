80dd6bc17750acaae766011715a1678bd8598816
rebs-code <jack.rebussi@gmail.com>
2024-01-15 18:03:52 +0100

    create: footer.php & add fontawesome cdn

--- index.php
+++ index.php
@@ -16,12 +16,15 @@ include __DIR__ . '/src/models/Genre.php';
     <title>PHP Movies</title>
     <!-- Bootstrap CND -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- FontAwesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- my css -->
     <link rel="stylesheet" href="./src/css/style.css">
 </head>
 
 <body>
     <?php include __DIR__ . '/src/partials/header.php'; ?>
+    <?php include __DIR__ . '/src/partials/footer.php'; ?>
 
 </body>
 
---
+++ src/partials/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <p>This is the footer.</p>
+    <ul>
+        <li class="social-icon"><a href="https://twitter.com">Twitter</a></li>
+        <li class="social-icon"><a href="https://facebook.com">Facebook</a></li>
+        <li class="social-icon"><a href="https://instagram.com">Instagram</a></li>
+    </ul>
+</footer>
